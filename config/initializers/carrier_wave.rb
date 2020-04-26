ace4cfaea369c8fe19539bef2f95447740172a38
shojikenta <ec2-user@ip-172-31-42-126.ap-northeast-1.compute.internal>
2020-04-26 13:18:49 +0000

    add gitignore

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,9 +5,8 @@ require 'carrierwave/storage/fog'
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
+      :region                => ENV['ap-northeast-1'],
       :aws_access_key_id     => ENV['AKIA5YYOUZYV5INDRJ5M'],
       :aws_secret_access_key => ENV['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']
     }

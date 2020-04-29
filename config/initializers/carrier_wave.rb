d1067f210a5cca51b3cb6c44ab2b2b8790c85fdc
shojikenta <ec2-user@ip-172-31-42-126.ap-northeast-1.compute.internal>
2020-04-29 12:08:35 +0000

    gitignoreの修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,11 +6,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],
-      :aws_access_key_id     => ENV['AKIA5YYOUZYV5INDRJ5M'],
-      :aws_secret_access_key => ENV['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'],
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['shoji2']
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/shoji2'
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
